05572916b04e080ef4b4698666c08f81519dbb3d
Lê Thanh Tuấn <tuanitpro@gmail.com>
2017-02-24 07:54:25 +0700

    commit all

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBase64toImage = new System.Windows.Forms.Button();
+            this.btnImageToBase64 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base64";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(651, 175);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Picture";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 292);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 206);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBase64toImage
+            // 
+            this.btnBase64toImage.Location = new System.Drawing.Point(15, 228);
+            this.btnBase64toImage.Name = "btnBase64toImage";
+            this.btnBase64toImage.Size = new System.Drawing.Size(127, 23);
+            this.btnBase64toImage.TabIndex = 4;
+            this.btnBase64toImage.Text = "Base64 to Image";
+            this.btnBase64toImage.UseVisualStyleBackColor = true;
+            this.btnBase64toImage.Click += new System.EventHandler(this.btnBase64toImage_Click);
+            // 
+            // btnImageToBase64
+            // 
+            this.btnImageToBase64.Location = new System.Drawing.Point(148, 228);
+            this.btnImageToBase64.Name = "btnImageToBase64";
+            this.btnImageToBase64.Size = new System.Drawing.Size(131, 23);
+            this.btnImageToBase64.TabIndex = 4;
+            this.btnImageToBase64.Text = "Image to Base64";
+            this.btnImageToBase64.UseVisualStyleBackColor = true;
+            this.btnImageToBase64.Click += new System.EventHandler(this.btnImageToBase64_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 515);
+            this.Controls.Add(this.btnImageToBase64);
+            this.Controls.Add(this.btnBase64toImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "http://tuanitpro.com";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBase64toImage;
+        private System.Windows.Forms.Button btnImageToBase64;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBase64toImage_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                Image img = Helper.Base64ToImage(richTextBox1.Text);
+                pictureBox1.Image = img;
+                if (img != null)
+                {
+                    SaveFileDialog sdg = new SaveFileDialog();
+                    sdg.Filter = "JPG (*.jpg)|*.jpg|PNG (*.png)|*.png|All files (*.*)|*.*";
+                    if (sdg.ShowDialog() == DialogResult.OK)
+                    {
+                        img.Save(sdg.FileName);
+                    }
+                }
+            }
+        }
+
+        private void btnImageToBase64_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "JPG (*.jpg)|*.jpg|PNG (*.png)|*.png|All files (*.*)|*.*";
+            if (dlg.ShowDialog()== DialogResult.OK)
+            {
+                pictureBox1.Load(dlg.FileName);
+                var image = Image.FromFile(dlg.FileName);
+                if (image != null)
+                {
+                    string base64 = Helper.ImageToBase64(image, ImageFormat.Jpeg);
+                    richTextBox1.Text = base64;
+                }
+            }
+        }
+    }
+}
